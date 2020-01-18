8692b4b1ec46f26917de3b02f4e1662bbcb171e0
Tomohiro <zealot6421@gmail.com>
2020-01-18 22:54:44 +0900

    Repair env

--- app/email.py
+++ app/email.py
@@ -1,3 +1,4 @@
+import os
 from threading import Thread
 from flask import render_template, current_app
 from flask_mail import Message, Mail
@@ -9,8 +10,8 @@ def send_email(to, subject, template, **kwargs):
     app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USERNAME'] = 'tomo18330228@gmail.com'
-    app.config['MAIL_PASSWORD'] = 'FJ18081wr'
+    app.config['MAIL_USERNAME'] = os.environ['MAIL_USERNAME']
+    app.config['MAIL_PASSWORD'] = os.environ['MAIL_PASSWORD']
     msg = Message(app.config['SAMPLE_MAIL_SUBJECT_PREFIX']+subject,
                   sender=app.config['FLASK_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
